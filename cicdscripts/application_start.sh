1dc5d288bb153c15878d14e3db05e1154128a15a
Gea Dragonaries <akosipoloyacero@gmail.com>
2021-05-22 19:08:06 +0400

    fixes

--- cicdscripts/application_start.sh
+++ cicdscripts/application_start.sh
@@ -5,9 +5,9 @@ cd /home/ubuntu/publishing-app
 sudo chmod -R 777 /home/ubuntu/publishing-app
 
 #add npm and node to path
-#export NVM_DIR="$HOME/.nvm"	
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #export NVM_DIR="/home/ubuntu/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
