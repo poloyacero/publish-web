72e8b40d4c965a30dcb6890ee7d4a1fa61a45e6f
gabzreal <balbuenagf@gmail.com>
2021-05-31 23:43:41 +0800

    alignment and bgcolor to light

--- pages/packages.tsx
+++ pages/packages.tsx
@@ -17,61 +17,51 @@ export default function PackagesPage() {
       </Head>
       <NavMenu />
       <Banner title="Packages" img="/img/8.png" variant="center" bg="#faf6f2">
-        <p>All-inclusive Self-Publishing Packages At A Great Deal!</p>
+        <p className={styles.bannerCenter}>All-inclusive Self-Publishing Packages At A Great Deal!</p>
       </Banner>
       <PageLayout>
         <Package title="Starter" price={900} width={3} currency="€">
           <p>
-            <b>Editorial and Design</b><br/>
-            25 Images Alloted<br/>
-            5  Image Enhancement<br/>
-            Standard Text and Layout Formatting<br/>
-            <b>Production features:</b><br/>
+            <b>Edit and Design</b><br/>
             Paperback Format<br/>
-            E-book Format<br/>
-            Track Book Production Progress<br/>
-            Electronic & Paper Galley<br/>
-            1 Round of Alterations & 25 Corrections<br/>
+            E-Book Format<br/>
+            Standard Text and Layout Formatting<br/>
+            1 Round Layout Revisions<br/>
             <b>Post-publication features:</b><br/>
             3 Paperback Copies<br/>
-            Barnes and Noble Read Instantly<br/>
+            2 ISBN<br/>
+            Print Book Registration<br/>
             Amazon Look Inside<br/>
             Google Books Preview<br/>
-            Registration with Books In Print<br/>
-            Assignment of International Standard Book Number (ISBN)<br/>
+            Barnes and Noble Read Instantly<br/>
             Worldwide Online Book Distribution<br/>
-            Book and Author featured in the Publishing Webpage<br/>
-            Online Book Sales and Royalty Accounting<br/>
-            Royalty Payments<br/>
+            Online Book Sales Account<br/>
+            Royalties<br/>
             <b>Marketing services:</b><br/>
-            20 Bookmarks
-            20 Business Cards
+            20 Bookmarks<br/>
+            20 Business Cards<br/>
             50 Postcards<br/>
+
+
           </p>
         </Package>
         <Package title="Deluxe" price={1500} width={3} currency="€">
           <p>
-            <b>Editorial and Design</b><br/>
-            50 Images Alloted<br/>
-            25 Image Enhancement<br/>
-            Standard Text and Layout Formatting<br/>
-            <b>Production features:</b><br/>
+          <b>Edit and Design</b><br/>
             Paperback Format<br/>
             E-Book Format<br/>
-            Track Book Production Progress<br/>
-            Electronic & Paper Galley<br/>
-            1 Round of Alterations & 25 Corrections <br/>
+            Standard Text and Layout Formatting<br/>
+            1 Round Layout Revisions<br/>
             <b>Post-publication features:</b><br/>
             5 Paperback Copies<br/>
-            Barnes and Noble Read Instantly<br/>
+            2 ISBN <br/>
+            Print Book Registration<br/>
             Amazon Look Inside<br/>
             Google Books Preview<br/>
-            Registration with Books In Print<br/>
-            Assignment of International Standard Book Number (ISBN)<br/>
+            Barnes and Noble Read Instantly<br/>
             Worldwide Online Book Distribution<br/>
-            Book and Author featured in the Publishing Webpage<br/>
-            Online Book Sales and Royalty Accounting<br/>
-            Royalty Payments<br/>
+            Online Book Sales Account<br/>
+            Royalties<br/>
             <b>Marketing services:</b><br/>
             30 Bookmarks<br/>
             30 Business Cards<br/>
@@ -82,90 +72,79 @@ export default function PackagesPage() {
         </Package>
         <Package title="Pro" price={4000} width={3} currency="€">
           <p>
-            <b>Editorial and Design</b><br/>
-            100  Images Alloted<br/>
-            50 Image Enhancement<br/>
-            Custom Text & Layout Formatting<br/>
-            Basic Cover Design<br/>
-            <b>Production features:</b><br/>
-            Paperback Format<br/>
-            Hardcover Format<br/>
-            E-Book Format<br/>
-            Design Consultation<br/>
-            Data Entry<br/>
-            Track Book Production Progress<br/>
-            Electronic & Paper Galley<br/>
-            2 Round of Alterations & 25 Corrections<br/>
-            Post-publication features:<br/>
-            15 Paperback Copies<br/>
-            3 Hardback Copies<br/>
-            10 BookStub Cards<br/>
-            Barnes and Noble Read Instantly<br/>
-            Amazon Look Inside<br/>
-            Google Books Preview<br/>
-            Registration with Books In Print<br/>
-            International Standard Book Number (ISBN)<br/>
-            Worldwide Online Book Distribution<br/>
-            Book and Author featured in the Publishing Webpage<br/>
-            Online Book Sales and Royalty Accounting<br/>
-            Royalty Payments<br/>
-            Copyright Registration<br/>
-            Bookstore Returnability - 12 Months<br/>
-            <b>Marketing services:</b><br/>
-            50 Bookmarks<br/>
-            50 Business Cards<br/>
-            50 Postcards<br/>
-            5 Posters<br/>
-            Basic Web Design<br/>
-            Press Release Campaign<br/>
+          <b>Edit and Design</b><br/>
+              Paperback Format<br/>
+              Hardcover Format<br/>
+              E-Book Format<br/>
+              Data Entry<br/>
+              Custom Text & Layout Formatting<br/>
+              2 Rounds Layout Revisions<br/>
+              Design Consultation<br/>
+              Basic Cover Design<br/>
+              <b>Post-publication features:</b><br/>
+              15 Paperback Copies<br/>
+              3 Hardback Copies<br/>
+              10 BookStub Cards<br/>
+              3 ISBN<br/>
+              Copyright Registration<br/>
+              Print Book Registration<br/>
+              Amazon Look Inside<br/>
+              Google Books Preview<br/>
+              Barnes and Noble Read Instantly<br/>
+              Worldwide Online Book Distribution<br/>
+              Online Book Sales Account<br/>
+              Royalties<br/>
+              Book Return Program  12 Months<br/>
+              <b>Marketing services:</b><br/>
+              50 Bookmarks<br/>
+              50 Business Cards<br/>
+              50 Postcards<br/>
+              5 Posters<br/>
+              Basic Web Design<br/>
+              Press Release Campaign <br/>
 
           </p>
         </Package>
         <Package title="Elite" price={16000} width={3} currency="€">
           <p>
-            <b>Editorial and Design</b><br/>
-            300 Images Alloted<br/>
-            150 Image Enhancement<br/>
-            Custom Text & Layout Formatting<br/>
-            Advance Cover Design<br/>
-            <b>Production features:</b><br/>
-            Availability of Your Book in Paperback Format<br/>
-            Availability of Your Book in Hardcover Format<br/>
-            Availability of Your Book in Color E-Book Format<br/>
-            Design Consultation<br/>
+          <b>Edit and Design</b><br/>
+            Paperback Format<br/>
+            Hardcover Format<br/>
+            E-Book Format<br/>
             Data Entry<br/>
             Copyediting<br/>
             Indexing<br/>
-            Citations - Footnotes & Endnotes<br/>
-            Track Book Production Progress<br/>
-            Electronic & Paper Galley<br/>
-            2 Rounds of Alterations & Corrections Service<br/>
-            Post-publication features:<br/>
+            Custom Text & Layout Formatting<br/>
+            2 Rounds Layout Revisions<br/>
+            Design Consultation<br/>
+            Advance Cover Design<br/>
+            <b>Post-publication features:</b><br/>
             50 Paperback Copies<br/>
-            10Hardback Copies<br/>
+            10 Hardback Copies<br/>
             20 BookStub Cards<br/>
-            2 Leather-Bound Edition – The Ambassador<br/>
-            Barnes and Noble Read Instantly<br/>
+            2 Leather-Bound <br/>
+            3 ISBN<br/>
+            Copyright Registration<br/>
+            Print Book Registration<br/>
             Amazon Look Inside<br/>
             Google Books Preview<br/>
-            Registration with Books In Print<br/>
-            International Standard Book Number (ISBN)<br/>
+            Barnes and Noble Read Instantly<br/>
             Worldwide Online Book Distribution<br/>
-            Book and Author featured in the Publishing Webpage<br/>
-            Online Book Sales and Royalty Accounting<br/>
-            Royalty Payments<br/>
-            Copyright Registration<br/>
-            100% Royalty Program - 3 years<br/>
-            Bookstore Returnability - 12 Months<br/>
+            Online Book Sales Account<br/>
+            Royalties<br/>
+            100% Royalty Program 3 years<br/>
+            Book Return Program  12 Months<br/>
             <b>Marketing services:</b><br/>
             200 Bookmarks<br/>
             200 Business Cards<br/>
             200 Postcards<br/>
             20 Posters<br/>
-            Advance Web Design<br/>
-            Press Release Campaign<br/>
-            Social Media Setup Guide<br/>
-            Book Video with Voice-Over<br/>
+            Advance Website<br/>
+            Press Release Campaign <br/>
+            Author Events<br/>
+            Social Media <br/>
+            Audio Book<br/>
+            Video Book<br/>
 
           </p>
         </Package>
