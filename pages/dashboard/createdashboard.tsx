df57e836e652e20f579700884b07ada0cdb03c67
Gabriel <balbuenagf@gmail.com>
2021-06-16 12:38:45 +0800

    rename file create

--- pages/dashboard/createdashboard.tsx
+++ pages/dashboard/createdashboard.tsx
@@ -5,35 +5,31 @@ import NavMenu from '../../components/Nav'
 import Sidebar from '../../components/Dashboard/Sidebar'
 import styles from '../../styles/Home.module.css'
 import styles2 from '../../components/Dashboard/NavMenu.module.css'
-// import DForms from '../../components/Dashboard/forms'
-// import styleIn from '../../styles/inputstyle.module.css'
-import { Col, Container, Row ,Form} from 'react-bootstrap'
+import { Col, Container, Row ,Image} from 'react-bootstrap'
 import Footer from "../../components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function createbook() {
+export default function homedashboard() {
     return (
         <div className={styles.container}>
       <Head>
-        <title>Create</title>
+        <title>Home Dashboard</title>
         <link rel="icon" href="/thepublishing.svg" />
       </Head>
       <NavMenu />
-      <NavMenuDashboard /> 
-      {/* <NavMenuDashboard className={styles.home}/>  */}
+      <NavMenuDashboard className={styles.home}/>  
+   
         <Container fluid>
           <Container>
-            <Row >
+            <Row className={styles2.dashcontainer} >
                <Sidebar/>
-                <Col md={6}>
-             <Col >
-             {/* <Form.Control className={styleIn.inputselect} as="select" defaultValue="Select Creation">
-                  <option disabled>Select Creation</option>                                
-                  <option>Print Book and Ebook € 350</option>
-                  <option>Print Book Only € 300</option>                  
-                  <option>Ebook Only € 250</option>  
-             </Form.Control> */}
-            {/* <DForms/> */}
+                <Col>
+             <Col className={styles2.homecontainer}>
+             <h2 className={styles.ctext}>Welcome to the Publishing!</h2>
+             <p><Image src="/img/homepic.png" width="438px" height="auto" /></p>
+
+             <p>How we can help you?<br/>
+             <span style={{fontSize:'20px'}}>+45 2090 1011</span></p>
              </Col> 
              </Col>
             </Row>
