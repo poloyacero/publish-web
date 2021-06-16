fff7e4e16156099b4cdc5c7db169a16070b201a2
Gabriel <balbuenagf@gmail.com>
2021-06-16 12:50:18 +0800

    add packages

--- pages/dashboard/homedashboard - Copy.tsx
+++ pages/dashboard/homedashboard - Copy.tsx
@@ -5,34 +5,33 @@ import NavMenu from '../../components/Nav'
 import Sidebar from '../../components/Dashboard/Sidebar'
 import styles from '../../styles/Home.module.css'
 import styles2 from '../../components/Dashboard/NavMenu.module.css'
-import { Col, Container, Row ,Form,Button} from 'react-bootstrap'
+import { Col, Container, Row ,Image} from 'react-bootstrap'
 import Footer from "../../components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Link from "next/link"
 
-export default function dashpackage() {
+export default function homedashboard() {
     return (
         <div className={styles.container}>
       <Head>
-        <title>Dash Package</title>
+        <title>Home Dashboard</title>
         <link rel="icon" href="/thepublishing.svg" />
       </Head>
       <NavMenu />
-      <NavMenuDashboard className={styles.home}/> 
+      <NavMenuDashboard className={styles.home}/>  
    
         <Container fluid>
           <Container>
             <Row className={styles2.dashcontainer} >
                <Sidebar/>
-             <Col md={6}>
-              <Col className={styles2.createcontainer}>
-              <Link href="/dashboard/starterpkg">Starter</Link>
-              <Link href="/dashboard/deluxepkg">Deluxe</Link>
-              <Link href="/dashboard/propkg">Pro</Link>
-              <Link href="/dashboard/elitepkg">Elite</Link>
-             </Col>      
-              </Col>
-  
+                <Col>
+             <Col className={styles2.homecontainer}>
+             <h2 className={styles.ctext}>Welcome to the Publishing!</h2>
+             <p><Image src="/img/homepic.png" width="438px" height="auto" /></p>
+
+             <p>How we can help you?<br/>
+             <span style={{fontSize:'20px'}}>+45 2090 1011</span></p>
+             </Col> 
+             </Col>
             </Row>
           </Container>   
        
