6f92705257fce8c8bb78cfa5cb7736018000712e
Gabriel <balbuenagf@gmail.com>
2021-06-16 17:39:18 +0800

    create dashboard

--- components/Dashboard/Sidebar.tsx
+++ components/Dashboard/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Row, Col, Button} from 'react-bootstrap';
 import Link from 'next/link';
-import styles from './NavMenu.module.css';
+import styles from './NavMenuDashboard.module.css';
 
 
 
--- pages/dashboard/homedashboard.tsx
+++ pages/dashboard/homedashboard.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Head from 'next/head'
-import NavMenuDashboard from '../../components/Dashboard/Nav'
+import NavMenuDashboard from '../../components/Dashboard/Navdashboard'
 import NavMenu from '../../components/Nav'
 import Sidebar from '../../components/Dashboard/Sidebar'
 import styles from '../../styles/Home.module.css'
-import styles2 from '../../components/Dashboard/NavMenu.module.css'
+import styles2 from '../../components/Dashboard/NavMenuDashboard.module.css'
 import { Col, Container, Row ,Image} from 'react-bootstrap'
 import Footer from "../../components/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
