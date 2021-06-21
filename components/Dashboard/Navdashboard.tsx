597945977eb587cb6a26b8c05d08dd37ee10e686
Gabriel <balbuenagf@gmail.com>
2021-06-21 19:11:32 +0800

    updated billing and titles page

--- components/Dashboard/Navdashboard.tsx
+++ components/Dashboard/Navdashboard.tsx
@@ -31,7 +31,7 @@ const Navdashboard = ({ ...props }: DNavProps) => {
                     </NavDropdown>    
                                  
                   <Link href="/dashboard/homedashboard"passHref><Nav.Link id={styles.menus} className={styles.navmenu}>Home</Nav.Link></Link>
-                  <Link href="/dashboard/servicesdashboard" passHref><Nav.Link id={styles.menus}  className={styles.navmenu}>Titles</Nav.Link></Link>
+                  <Link href="/dashboard/titlesdashboard" passHref><Nav.Link id={styles.menus}  className={styles.navmenu}>Titles</Nav.Link></Link>
                   <Link href="/dashboard/reportdashboard" passHref><Nav.Link id={styles.menus}  className={styles.navmenu}>Reports</Nav.Link></Link>
                   <Link href="/dashboard/ordersdashboard" passHref><Nav.Link id={styles.menus}  className={styles.navmenu}>Orders</Nav.Link></Link>
             </Navbar>    
