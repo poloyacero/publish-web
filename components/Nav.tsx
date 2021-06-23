3d74fc1b7b78407c1b8867ce58d75993b8eff6ac
Gabriel <balbuenagf@gmail.com>
2021-06-23 21:38:25 +0800

    transfer homedashboard page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -50,8 +50,8 @@ const NavMenu = ({ ...props }: NavProps) => {
           <Col md={{ span: 2, offset: 0, order: 'last'}} className={styles.utilityMenu}>
              <Col className={styles.utilitycontainer}>
                <Col>
-            <Link href="/dashboard/homedashboard" passHref>
-              <Nav.Link className={styles.utilityMenus} href="/dashboard/homedashboard">
+            <Link href="/homedashboard" passHref>
+              <Nav.Link className={styles.utilityMenus}>
                 <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" className="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path fillRule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
                   <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
