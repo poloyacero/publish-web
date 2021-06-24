fa8a826ccbf6a3eee73e3680741b793ed261ed18
Gabriel <balbuenagf@gmail.com>
2021-06-24 15:47:12 +0800

    add create in subfolder

--- components/Dashboard/Sidebar.tsx
+++ components/Dashboard/Sidebar.tsx
@@ -16,7 +16,7 @@ const Sidebar = ({ ...props }: SidebarProps) => {
     <Col md={4} className={styles.sidebarcol} >
     <Col className={styles.buttoncontainer}>
 <Row className={styles.buttonwrapper}>
-<Col><Link href="/dashboard/createdashboard" passHref><Button className={styles.button}>Create</Button></Link></Col>
+<Col><Link href="/dashboard/sidemenu/createdashboard" passHref><Button className={styles.button}>Create</Button></Link></Col>
 <Col><Link href="/dashboard/packagesdashboard" passHref><Button className={styles.button}>Package</Button></Link></Col>
 <Col><Link href="/dashboard/servicesdashboard" passHref><Button className={styles.button}>Services</Button></Link></Col>
 </Row> 
