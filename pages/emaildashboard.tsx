8b7fa185067c6480e024dbcdcb01cd645c89d17b
Gabriel <balbuenagf@gmail.com>
2021-06-25 20:45:39 +0800

    remove classNames

--- pages/emaildashboard.tsx
+++ pages/emaildashboard.tsx
@@ -7,31 +7,31 @@ import styles2 from '../components/Dashboard/NavMenuDashboard.module.css'
 import styleIn from '../styles/inputstyle.module.css'
 import { Col, Container, Row ,Form,Button} from 'react-bootstrap'
 import FooterDashboard from "../components/FooterDashboard";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function emaildashboard() {
     return (
-        <div className={styles.container}>
+        <div className={styles.containerx}>
       <Head>
         <title>Email</title>
         <link rel="icon" href="/thepublishing.svg" />
       </Head>
       <NavMenu />
-      {/* <NavMenuDashboard className={styles.home}/>  */}
+     
    
         <Container fluid>
-          <Container  className={styles.ctext}>
-            <Row className={styles2.accountinfocontainer} >
+          <Container>
+            <Row>
              
                 <Col>
-                <h3 className={styles2.homeHeaderlabel2}>Email</h3>
+                <h3>Email</h3>
                 <br></br>
                 <Row className="form-group text-left">
               <Col ><Form.Control as="textarea" rows={15}/> </Col>
                  </Row>
                  <br/> <br/> <br/> 
               <Row className="form-group text-left">
-            <Col md={2}><Button className={styles2.savebutton}>Send</Button></Col>
+            <Col md={2}><Button>Send</Button></Col>
             </Row>
               <br/> <br/> <br/>
              </Col>
@@ -43,5 +43,5 @@ export default function emaildashboard() {
      
       <FooterDashboard />    
         </div>
-    )
+    );
 }
