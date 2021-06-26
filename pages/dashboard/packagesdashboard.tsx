929e1323ad589be10ebb23b42a601fd3cfc85580
Gabriel <balbuenagf@gmail.com>
2021-06-27 06:37:42 +0800

    add navbar dashboard css

---
+++ pages/dashboard/packagesdashboard.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from "next/link"
+import NavMenuDashboard from '../../components/Dashboard/Navdashboard'
+import NavMenu from '../../components/Nav'
+import Sidebar from '../../components/Dashboard/Sidebar'
+import styles from '../../styles/Home.module.css'
+import styles2 from '../../components/Dashboard/dashboard.module.css'
+import { Col, Container, Row} from 'react-bootstrap'
+import FooterDashboard from "../../components/FooterDashboard";
+
+
+export default function dashpackage() {
+    return (
+        <div className={styles.container}>
+      <Head>
+        <title>Dashboard Packages</title>
+        <link rel="icon" href="/thepublishing.svg" />
+      </Head>
+      <NavMenu />
+      <NavMenuDashboard className={styles.home}/> 
+   
+        <Container fluid>
+          <Container>
+            <Row>
+               <Sidebar/>
+             <Col md={6}>
+             
+              <Col className={styles2.homecontainer+" "+styles.serviceslink}>
+              
+              <h4 className={styles2.homeheaderlabel}><Link href="/dashboard/starterpkg" passHref>Starter</Link></h4>               
+             
+             
+              <h4 className={styles2.homeheaderlabel}> <Link href="/dashboard/deluxepkg" passHref>Deluxe</Link></h4>               
+          
+              
+              <h4 className={styles2.homeheaderlabel}><Link href="/dashboard/propkg" passHref>Pro</Link></h4>
+              
+              
+              <h4 className={styles2.homeheaderlabel}><Link href="/dashboard/elitepkg" passHref>Elite</Link></h4>                
+             
+             </Col>      
+              </Col>
+  
+            </Row>
+          </Container>   
+       
+        </Container>
+     
+      <FooterDashboard />    
+        </div>
+    )
+}
