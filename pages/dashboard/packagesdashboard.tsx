76cc49939656e0278ba8e05cf175325c228978a3
Gabriel <balbuenagf@gmail.com>
2021-06-27 04:26:04 +0800

    add navbar dashboard css

--- pages/dashboard/packagesdashboard.tsx
+++ pages/dashboard/packagesdashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import Link from "next/link"
 import NavMenuDashboard from '../../components/Dashboard/Navdashboard'
 import NavMenu from '../../components/Nav'
 import Sidebar from '../../components/Dashboard/Sidebar'
@@ -7,8 +8,7 @@ import styles from '../../styles/Home.module.css'
 import styles2 from '../../components/Dashboard/NavMenuDashboard.module.css'
 import { Col, Container, Row} from 'react-bootstrap'
 import FooterDashboard from "../../components/FooterDashboard";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Link from "next/link"
+
 
 export default function dashpackage() {
     return (
@@ -28,16 +28,16 @@ export default function dashpackage() {
              
               <Col className={styles2.xcontainer+" "+styles.serviceslink}>
               
-              <h4 className={styles2.homeheaderlabel}><Link href="/dashboard/starterpkg" passHref>Starter</Link></h4>               
+              <h4 className={styles2.xheaderlabel}><Link href="/dashboard/starterpkg" passHref>Starter</Link></h4>               
              
              
-              <h4 className={styles2.homeheaderlabel}> <Link href="/dashboard/deluxepkg" passHref>Deluxe</Link></h4>               
+              <h4 className={styles2.xheaderlabel}> <Link href="/dashboard/deluxepkg" passHref>Deluxe</Link></h4>               
           
               
-              <h4 className={styles2.homeheaderlabel}><Link href="/dashboard/propkg" passHref>Pro</Link></h4>
+              <h4 className={styles2.xheaderlabel}><Link href="/dashboard/propkg" passHref>Pro</Link></h4>
               
               
-              <h4 className={styles2.homeheaderlabel}><Link href="/dashboard/elitepkg" passHref>Elite</Link></h4>                
+              <h4 className={styles2.xheaderlabel}><Link href="/dashboard/elitepkg" passHref>Elite</Link></h4>                
              
              </Col>      
               </Col>
